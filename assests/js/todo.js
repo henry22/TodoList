97448531b6237f6bb82e812932671ea0838b4de8
YuCheng Chien <henry811010@gmail.com>
2017-01-19 14:32:18 +0800

    First commit

---
+++ assests/js/todo.js
@@ -0,0 +1,28 @@
+// Check Off specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("lineDelete");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+	
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//clean input text
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+	
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+	
